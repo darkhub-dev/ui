cedc4073e32fab5928ad251d55ac2a78ac28232e
abdalrahmansameh <darkness.itec.eg@gmail.com>
2025-08-19 04:01:41 +0300

    Enhance header styling with gradient text effect in the main page component.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = () => {
   return (
     <main className='relative flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl font-bold z-50'>Hello World !</h1>
+      <h1 className='text-4xl font-bold z-50 text-transparent top-96 bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500'>Hello World !</h1>
 
       <Test />
 
